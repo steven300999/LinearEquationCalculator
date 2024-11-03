356d140eac53b124c0b7a93182fc50b7b04db56e
abacu <sample@bths.edu>
2024-11-03 11:50:03 -0500

    V2

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -10,6 +10,12 @@ public class LinearEquation {
         this.x2 = p2.x;
         this.y2 = p2.y;
     }
+    public LinearEquation(int x1, int y1, int x2, int y2) {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+    }
 
     public double distance() {
         return Math.round(Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)) * 100.0) / 100.0;
@@ -19,47 +25,78 @@ public class LinearEquation {
         return Math.round((y2 - y1) * (x1 - x2) * 100.0) / 100.0;
     }
 
-    public double slope() {
+    public String slope() {
         if (x2 == x1) {
-            return 0;
+            return "∞"; // Represent vertical line as infinity
         }
-        return Math.round((y2 - y1) * 100.0) / ((x2 - x1) * 100.0);
+        int numerator = y2 - y1;
+        int denominator = x2 - x1;
+        return simplifyFraction(numerator, denominator);
+    }
+
+    private String simplifyFraction(int numerator, int denominator) {
+        int gcd = calculateGCD(Math.abs(numerator), Math.abs(denominator));
+        int simplifiedNumerator = numerator / gcd;
+        int simplifiedDenominator = denominator / gcd;
+
+        if (simplifiedDenominator == 1) {
+            return Integer.toString(simplifiedNumerator);
+        }
+        return simplifiedNumerator + "/" + simplifiedDenominator;
+    }
+
+    private int calculateGCD(int a, int b) {
+        if (b == 0) {
+            return a;
+        }
+        return calculateGCD(b, a % b);
     }
 
     public String equation() {
-        double slope = slope();
+        String slopeStr = slope();
         double yInt = yIntercept();
 
-        if (Double.isNaN(slope)) {
+        if (slopeStr.equals("∞")) {
             return "Vertical line";
         }
 
-        if (slope == 0) {
+        if (slopeStr.equals("0")) {
             return "Horizontal line";
         }
 
-        if (slope == 1) {
-            return "y = x + " + yInt;
-        }
-
-        if (slope == -1) {
-            return "-y = x + " + yInt;
-        }
-
         if (yInt == 0) {
-            return "y = " + slope + "x";
+            return "y = " + slopeStr + "x";
         }
 
         if (yInt < 0) {
-            return "y = " + slope + "x - " + Math.abs(yInt);
+            return "y = " + slopeStr + "x - " + Math.abs(yInt);
         }
 
-        return "y = " + slope + "x + " + yInt;
+        return "y = " + slopeStr + "x + " + yInt;
     }
 
     public String coordinateForX(double x) {
-        double y = yIntercept() + slope() * x;
+        double y = yIntercept() + parseSlope(slope()) * x;
         return "(" + x + ", " + Math.round(y * 100.0) / 100.0 + ")";
+    }
+
+    private double parseSlope(String slopeStr) {
+        if (slopeStr.equals("∞")) {
+            throw new ArithmeticException("Cannot calculate coordinates for vertical line");
+        }
+
+        int numerator = 0;
+        int denominator = 1;
+
+        if (slopeStr.contains("/")) {
+            String[] parts = slopeStr.split("/");
+            numerator = Integer.parseInt(parts[0]);
+            denominator = Integer.parseInt(parts[1]);
+        } else {
+            numerator = Integer.parseInt(slopeStr);
+        }
+
+        return (double) numerator / denominator;
     }
 
     public String lineInfo() {
@@ -70,4 +107,5 @@ public class LinearEquation {
                 "The distance between these points is: " + distance();
     }
 }
+
 
